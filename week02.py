12c4651faa202ff3d3fc938e7c0a3158656f7a8b
jinyong <jinyong2527@gmail.com>
2026-03-12 16:21:47 +0900

    week02 v0.3

--- week02.py
+++ week02.py
@@ -1,9 +1,4 @@
+import statistics
 scores = [100, 97, 88, 91]
-hap = 0
-count = 0
-for score in scores:
-    hap = hap + score
-    count = count + 1
-
-average = hap / count
+average = statistics.mean(scores)
 print(average)
