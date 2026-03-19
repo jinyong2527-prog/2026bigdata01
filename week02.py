22fb22194d7a54289e0f0693aa5275bec7d05967
jinyong <jinyong2527@gmail.com>
2026-03-19 14:38:06 +0900

    week03 v0.4

--- week02.py
+++ week02.py
@@ -1,4 +1,5 @@
 import statistics
-scores = [100, 97, 88, 91]
-average = sum(scores) / len(scores)
+scores = [100, 97, 88, 91]# 써드파티 라이브러리,C배열 기반으로
+                        # 대규모 데이터 처리에 적합, 매우 빠름
+average = statistics.mean(scores)
 print(average)
---
+++ week03.py
@@ -0,0 +1,5 @@
+import numpy
+
+scores = [100, 97, 88, 91] #통계모듈 (각종 통계함수를 제공), 파이썬으로
+average = numpy.mean(scores)
+print(average)
