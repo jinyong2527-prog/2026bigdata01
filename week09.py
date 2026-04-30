cb1894b592e7074ea5963a9e15fa6630e460d13a
jinyong <jinyong2527@gmail.com>
2026-04-30 14:39:13 +0900

    week09 pandas v0.1

--- week09.py
+++ week09.py
@@ -3,12 +3,16 @@ import pandas as pd
 
 arr2d = np.array(
     [
-        [1,2,3],
-        [4,5,6],
-        [7,8,9]
+        [1, 2, 3],
+        [6, 4, 5],
+        [7, 8, 9]
     ]
 )
+# df_2dlist = pd.DataFrame(arr2d, columns=['국', '영', '수'], index=[1, 2, 3])
 
-df_dict = pd.DataFrame(arr2d, columns=['a','b','c'],index=[1,2,3])
-print(df_dict)
+# Creating Dataframes
+df_2dlist = pd.DataFrame([[1, 2, 3], [6, 4, 5], [7, 8, 9]], columns=['국', '영', '수'], index=[1, 2, 3])
+df_dict = pd.DataFrame({'국':[1, 6, 7], '영':[2, 4, 8], '수':[3, 5, 9]}, index=[1, 2, 3])
 print(arr2d)
+print(df_2dlist)
+print(df_dict)
