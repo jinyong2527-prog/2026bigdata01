68e672f460ddec2a907f6b38c4b668c1b7246d4d
jinyong <jinyong2527@gmail.com>
2026-04-30 14:51:42 +0900

    week09 pandas v0.2

--- week09.py
+++ week09.py
@@ -16,3 +16,10 @@ df_dict = pd.DataFrame({'국':[1, 6, 7], '영':[2, 4, 8], '수':[3, 5, 9]}, inde
 print(arr2d)
 print(df_2dlist)
 print(df_dict)
+
+df = pd.DataFrame({'국':[1, 6, 7], '영':[2, 4, 8], '수':[3, 5, 9]}, index=[1, 2, 3])
+print(df)
+# df_new = pd.melt(df)
+# df_new = pd.melt(df).rename(columns={'variable': 'var', 'value': 'val'})
+df_new = pd.melt(df).rename(columns={'variable': 'var', 'value': 'val'}).query('val > 5')
+print(df_new)
